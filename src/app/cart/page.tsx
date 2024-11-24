47c8052da667d996e87df182751b202cdc50b766
peter varga <pkvarg@yahoo.se>
2024-11-24 20:46:40 +0100

    cart ok

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,24 +1,20 @@
 'use client'
 import React, { useState } from 'react'
 import useCartStore from '@/store/cartStore'
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card'
-import { Button } from '@react-email/components'
+import { CardDescription, CardTitle } from '@/components/ui/card'
 import { formatCurrency } from '@/lib/formatters'
 import Image from 'next/image'
 import { Nav, NavLink } from '@/components/Nav'
 import { BsTrash } from 'react-icons/bs'
+import { useRouter } from 'next/navigation'
 
 const Cart = () => {
+  const router = useRouter()
+
+  const goToHome = () => {
+    router.push('/') // Navigate to the home page
+  }
   const { items, updateItemQty, removeFromCart } = useCartStore((state) => state)
-  //const [count, setCount] = useState(1)
-  // Thrash delete from cart
 
   const increment = (id: string, qty: number) => {
     updateItemQty(id, qty + 1)
@@ -30,14 +26,21 @@ const Cart = () => {
     } else updateItemQty(id, 1)
   }
 
+  function roundUpToNearestTenth(price: number) {
+    return Math.ceil(price * 10) / 10
+  }
+
   const totalItemsQty = items.map((item) => item.qty).reduce((total, qty) => total + qty, 0) // Sum them up
 
   const totalItemsPrice =
     items.reduce((total, item) => total + item.priceInCents * item.qty, 0) / 100
 
-  console.log('items cart', items)
-  console.log('total items qty', totalItemsQty)
-  console.log('total items price', totalItemsPrice)
+  // TODO postage, tax to .env
+  const postage = 5
+  const tax = 25
+  const total = totalItemsPrice + 5
+  const taxFromTotal = (total * 25) / 100
+  const totalWithTax = roundUpToNearestTenth(total + taxFromTotal).toFixed(2)
 
   return (
     <div>
@@ -47,8 +50,19 @@ const Cart = () => {
         <NavLink href="/orders">My Orders</NavLink>
       </Nav>
       <h1 className="text-center my-4 text-[32px]">Your Cart</h1>
-      <div className="flex flex-row gap-24">
-        <div className="flex flex-col justify-center">
+      <div className="flex flex-col lg:flex-row gap-4 mx-4 lg:mx-[10%]">
+        <div className="flex flex-col lg:w-[75%]">
+          {items.length === 0 && (
+            <div className="flex flex-col justify-center items-center mt-16">
+              <h1 className="text-[25px] text-center">Your Cart is Empty</h1>
+              <button
+                onClick={goToHome}
+                className="bg-blue-500 text-gray-50 p-2 mt-2 cursor-pointer hover:bg-blue-800 "
+              >
+                Start shopping
+              </button>
+            </div>
+          )}
           {items.map((item) => (
             <div key={item.id} className="flex overflow-hidden flex-row my-8">
               <div className="relative m-2">
@@ -62,7 +76,7 @@ const Cart = () => {
 
                   {formatCurrency(item.priceInCents / 100)}
                 </CardDescription>
-                <div className="flex gap-4 ml-auto -mt-4 items-center">
+                <div className="flex gap-4 ml-[62.5%] lg:ml-auto -mt-4 items-center">
                   <p onClick={() => decrement(item.id, item.qty)} className="cursor-pointer">
                     -
                   </p>
@@ -81,9 +95,29 @@ const Cart = () => {
         </div>
 
         {/* cart totals */}
-        <div className="bg-gray-400">
-          <p className="font-bold">Total Items: ({totalItemsQty})</p>
-          <p className="font-bold">Products: {totalItemsPrice} &#8364;</p>
+        <div
+          className={
+            items.length > 0
+              ? 'mt-0 bg-gray-100 lg:w-[25%] h-fit lg:mt-0'
+              : 'mt-16  bg-gray-100 lg:w-[25%] h-fit lg:mt-0'
+          }
+        >
+          <div className="mx-4 my-4">
+            <p className="font-bold mt-2">Total Items: ({totalItemsQty})</p>
+            <p className="font-bold mt-2">Products: {totalItemsPrice}&#8364;</p>
+            <p className="font-bold mt-2">Postage: {totalItemsQty && postage}&#8364;</p>
+            <p className="font-bold mt-2">Tax: {totalItemsQty && tax}%</p>
+            <p className="font-bold mt-2">Total: {totalItemsQty && totalWithTax}&#8364;</p>
+            <button className="bg-gray-500 text-gray-50 p-2 mt-2 w-full cursor-pointer hover:bg-gray-700">
+              Go to Checkout
+            </button>
+            <button
+              onClick={goToHome}
+              className="bg-blue-500 text-gray-50 p-2 mt-2 w-full cursor-pointer hover:bg-blue-800"
+            >
+              Continue shopping
+            </button>
+          </div>
         </div>
       </div>
     </div>
