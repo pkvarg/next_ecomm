20b28c149f34ac85c7f9588fef2dfd31b7e74ccd
peter varga <pkvarg@yahoo.se>
2025-02-02 13:03:49 +0100

    search by name or description

--- src/app/(customerFacing)/page.tsx
+++ src/app/(customerFacing)/page.tsx
@@ -43,8 +43,10 @@ export default function HomePage() {
         setFilteredProducts(allProducts)
       } else {
         setFilteredProducts(
-          allProducts.filter((product) =>
-            product.name.toLowerCase().includes(searchQuery.toLowerCase()),
+          allProducts.filter(
+            (product) =>
+              product.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+              product.description?.toLowerCase().includes(searchQuery.toLowerCase()),
           ),
         )
       }
@@ -54,7 +56,7 @@ export default function HomePage() {
   return (
     <main className="space-y-12">
       <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
-      <ProductGridSection title="All Products" products={filteredProducts} />
+      <ProductGridSection title="" products={filteredProducts} />
       {/* <ProductGridSection title="Most Popular" productsFetcher={getMostPopularProducts} />
       <ProductGridSection title="Newest" productsFetcher={getNewestProducts} /> */}
     </main>
@@ -69,7 +71,7 @@ type ProductGridSectionProps = {
 function ProductGridSection({ title, products }: ProductGridSectionProps) {
   return (
     <div className="space-y-4">
-      <div className="flex gap-4">
+      {/* <div className="flex gap-4">
         <h2 className="text-3xl font-bold">{title}</h2>
         <Button variant="outline" asChild>
           <Link href="/products" className="space-x-2">
@@ -77,7 +79,7 @@ function ProductGridSection({ title, products }: ProductGridSectionProps) {
             <ArrowRight className="size-4" />
           </Link>
         </Button>
-      </div>
+      </div> */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {products.length > 0 ? (
           products.map((product) => <ProductCard key={product.id} {...product} />)
